306c5cfbd714c03fd2d88a31d69b087948e598d6
Princia Fernandes <148257736+PrinciaFernandes@users.noreply.github.com>
2024-12-02 23:42:23 +0530

    Add files via upload

---
+++ Dashboard.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px 
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import warnings
+warnings.filterwarnings("ignore")
+
+
+st.set_page_config(
+    page_title = "Multipage app"
+)
+
+st.title("Stock Price Dashboard")
+comp = st.selectbox("Select a Company",['Bajaj Finance Ltd.','HDFC Bank Ltd.','Maruti Suzuki India Ltd.','Reliance Industries Ltd.','Tata Consultancy Services Ltd.'],index = None)
+
+if comp:
+    data = pd.read_csv(rf'''C:\Users\Princia\Stock price prediction\datasets_stocks\{comp}.csv''')
+    data.sort_values(by = 'Date ')
+
+    st.subheader('Closing Price Over Time')
+    fig = px.line(data, x='Date ', y='close ')
+    st.plotly_chart(fig)
+    st.write("")
+
+    st.subheader('Volume of Trades Over Time')
+    fig = px.bar(data, data['Date '], data['VOLUME '],color = 'VOLUME ')
+    st.plotly_chart(fig)
+    st.write('')
+
+    st.subheader('Distribution of Closing Prices')
+    fig = px.histogram(data, x='close ', nbins =50,  hover_data=data.columns )
+    fig.update_layout(bargap = 0.2 ,width=1000, height=400)
+    st.plotly_chart(fig)
+
+    st.subheader('High and Low Prices Over Time')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data['HIGH '], mode='lines', name='High Price', line=dict(color='green')))
+    fig.add_trace(go.Scatter(x=data.index, y=data['LOW '], mode='lines', name='Low Price', line=dict(color='red')))
+    fig.update_layout(title='', xaxis_title='Date', yaxis_title='Price', title_font_size=24, xaxis_title_font_size=18, yaxis_title_font_size=18)
+    st.plotly_chart(fig)
+
+    st.subheader('Candlestick Chart')
+    fig = go.Figure(data=[go.Candlestick(x=data['Date '],
+                                        open=data['OPEN '],
+                                        high=data['HIGH '],
+                                        low=data['LOW '],
+                                        close=data['close '],
+                                        name='Candlestick')])
+
+    fig.update_layout(title='', xaxis_title='Date ', yaxis_title='Price ')
+    st.plotly_chart(fig)
