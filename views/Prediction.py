ec9b68f813f56e736298d32b98e5610e12bf94bd
Princia Fernandes <148257736+PrinciaFernandes@users.noreply.github.com>
2024-12-04 16:41:07 +0530

    Update Prediction.py

--- views/Prediction.py
+++ views/Prediction.py
@@ -15,8 +15,8 @@ st.text('')
 bajaj = pd.read_csv(r"model_data/datasets/Bajaj.csv")
 hdfc  = pd.read_csv(r"model_data/datasets/HDFC.csv")
 Maruti = pd.read_csv(r"model_data/datasets/Maruti.csv")
-reliance = pd.read_csv(r"/model_data/datasets/Reliance.csv")
-TATA = pd.read_csv(r"/model_data/datasets/TATA.csv")
+reliance = pd.read_csv(r"model_data/datasets/Reliance.csv")
+TATA = pd.read_csv(r"model_data/datasets/TATA.csv")
 
 
 def model_loader(name):
