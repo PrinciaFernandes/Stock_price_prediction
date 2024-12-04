30174edf3b137624a23ac0f6165c8cdee14f9225
Akash H N <148257396+AkashHN002@users.noreply.github.com>
2024-12-04 16:37:12 +0530

    Update Dashboard.py

--- views/Dashboard.py
+++ views/Dashboard.py
@@ -12,7 +12,7 @@ st.set_page_config(
      page_icon= "💲"
 )
 
-bajaj = pd.read_csv(r"model_data\datasets\Bajaj.csv")
+bajaj = pd.read_csv(r"model_data/datasets/Bajaj.csv")
 hdfc  = pd.read_csv(r"model_data\datasets\HDFC.csv")
 Maruti = pd.read_csv(r"\model_data\datasets\Maruti.csv")
 reliance = pd.read_csv(r"\model_data\datasets\Reliance.csv")
