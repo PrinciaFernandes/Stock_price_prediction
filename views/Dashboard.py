c6c1dd9ee15d55aa488f0c8b6ab92f48f99e0c2c
Princia Fernandes <princiafer2002@gmail.com>
2024-12-04 13:01:36 +0530

    added views

---
+++ views/Dashboard.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px 
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import warnings
+warnings.filterwarnings("ignore")
+
+
+st.set_page_config(
+     page_title = "Stock Price Page",
+     page_icon= "💲"
+)
+
+
+bajaj = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\Bajaj.csv")
+hdfc  = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\HDFC.csv")
+Maruti = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\Maruti.csv")
+reliance = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\Reliance.csv")
+TATA = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\TATA.csv")
+
+
+Dataset = {"Bajaj Finance Ltd.":bajaj,
+            "HDFC Bank Ltd." : hdfc,
+            "Maruti Suzuki India Ltd." : Maruti,
+            "Reliance Industries Ltd." : reliance,
+            "Tata Consultancy Services Ltd.": TATA}
+
+st.title("Stock Price Dashboard")
+comp = st.selectbox("Select a Company",['Bajaj Finance Ltd.','HDFC Bank Ltd.','Maruti Suzuki India Ltd.','Reliance Industries Ltd.','Tata Consultancy Services Ltd.'],index = None)
+
+if comp:
+    data = Dataset[comp]
+    data.sort_values(by = 'Date ')
+
+    st.subheader('Closing Price Over Time')
+    fig = px.line(data, x='Date ', y='close ')
+    st.plotly_chart(fig)
+    st.write("")
+
+    st.subheader('Volume of Trades Over Time')
+    fig = px.bar(data, data['Date '], data['VOLUME '],color = 'VOLUME ')
+    st.plotly_chart(fig)
+    st.write('')
+
+    st.subheader('Distribution of Closing Prices')
+    fig = px.histogram(data, x='close ', nbins =50,  hover_data=data.columns )
+    fig.update_layout(bargap = 0.2 ,width=1000, height=400)
+    st.plotly_chart(fig)
+
+    st.subheader('High and Low Prices Over Time')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data['HIGH '], mode='lines', name='High Price', line=dict(color='green')))
+    fig.add_trace(go.Scatter(x=data.index, y=data['LOW '], mode='lines', name='Low Price', line=dict(color='red')))
+    fig.update_layout(title='', xaxis_title='Date', yaxis_title='Price', title_font_size=24, xaxis_title_font_size=18, yaxis_title_font_size=18)
+    st.plotly_chart(fig)
+
+    st.subheader('Candlestick Chart')
+    fig = go.Figure(data=[go.Candlestick(x=data['Date '],
+                                        open=data['OPEN '],
+                                        high=data['HIGH '],
+                                        low=data['LOW '],
+                                        close=data['close '],
+                                        name='Candlestick')])
+
+    fig.update_layout(title='', xaxis_title='Date ', yaxis_title='Price ')
+    st.plotly_chart(fig)
---
+++ views/Prediction.py
@@ -0,0 +1,124 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import tensorflow as tf
+import sklearn
+from datetime import datetime, timedelta
+import warnings
+warnings.filterwarnings("ignore")
+import plotly.graph_objects as go
+
+st.title("Stock Price Prediction")
+st.text('')
+
+bajaj = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\Bajaj.csv")
+hdfc  = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\HDFC.csv")
+Maruti = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\Maruti.csv")
+reliance = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\Reliance.csv")
+TATA = pd.read_csv(r"C:\Users\Princia\Stock price prediction\model_data\datasets\TATA.csv")
+
+
+def model_loader(name):
+    model = tf.keras.models.load_model(rf"C:\Users\Princia\Stock price prediction\model_data\models\{name}\{name}_model.h5")
+    return model
+
+def scaler_loader(name):
+    with open(rf"C:\Users\Princia\Stock price prediction\model_data\models\{name}\{name}_scaler.pkl", 'rb') as f:
+        scaler = pickle.load(f)
+    return scaler
+
+window_size = 60
+def create_sequences(data, window_size):
+    sequences = []
+    sequences.append(data[0:0 + window_size])
+    return np.array(sequences)
+
+Dataset = {"Bajaj Finance Ltd.":bajaj,
+            "HDFC Bank Ltd." : hdfc,
+            "Maruti Suzuki India Ltd." : Maruti,
+            "Reliance Industries Ltd." : reliance,
+            "Tata Consultancy Services Ltd.": TATA}
+
+models = {
+    "Bajaj Finance Ltd.":[model_loader("Bajaj") , scaler_loader("Bajaj")],
+    "HDFC Bank Ltd." :[model_loader("HDFC") , scaler_loader("HDFC")],
+    "Maruti Suzuki India Ltd." : [model_loader("Maruti") , scaler_loader("Maruti")],
+    "Reliance Industries Ltd.": [model_loader("Reliance") , scaler_loader("Reliance")],
+    "Tata Consultancy Services Ltd." : [model_loader("TATA") , scaler_loader("TATA")]
+}
+
+comp = st.selectbox("Select a Company",['Bajaj Finance Ltd.','HDFC Bank Ltd.','Maruti Suzuki India Ltd.','Reliance Industries Ltd.','Tata Consultancy Services Ltd.'],index = None)
+
+if comp != None:
+    model = models[comp][0]
+    scaler = models[comp][1]
+
+    dataset = Dataset[comp]
+
+    col1,col2,col3,col4,col5 = st.columns(5)
+    with col1:
+        st.write("Prev. Close")
+        prev_close = dataset['PREV. CLOSE '].iloc[-1]
+        st.button(str(prev_close),key = 1)
+    with col2:
+        st.write("Open")
+        open = dataset['OPEN '].iloc[-1]
+        st.button(str(open),key = 2)
+    with col3:
+        st.write("High")
+        high = dataset['HIGH '].iloc[-1]
+        st.button(str(high),key = 3)
+    with col4:
+        st.write("Low")
+        low = dataset['LOW '].iloc[-1]
+        st.button(str(low),key = 4)
+    with col5:
+        st.write("VWAP")
+        vwap = dataset['vwap '].iloc[-1]
+        st.button(str(vwap),key = 5)
+    st.write(" ")
+
+
+    n = st.text_input("Enter the no. of days you want to get the prediction",None)
+    b = st.button(" Predict",use_container_width=True,key = 10)
+    if n != None:
+        n = int(n)
+        
+        if b:
+            price = scaler.transform(dataset.iloc[:,6].values.reshape(-1,1))
+            pred = [dataset.iloc[-1,6]]
+            window_size = 60
+            df = price[len(price) - window_size : ]
+            d = pd.to_datetime(dataset.iloc[-1,0], format="%Y-%m-%d")
+            date_ls = [str(d.date())]
+            for i in range(n):
+                sequence = create_sequences(df[-window_size:], window_size)[-1]
+                sequence = sequence.reshape(1, window_size, 1)
+                # Make the prediction
+                prediction = model.predict(sequence)
+                df = np.append(df.reshape(-1), prediction)
+                prediction = scaler.inverse_transform(prediction)
+
+                d += timedelta(days=1)
+                date_ls.append(str(d.date()))
+                pred.append(prediction[0][0])
+
+            line1 = go.Scatter(x=dataset.iloc[-10:,0], y=dataset.iloc[-10:,6], mode='lines', name='Prev. data')
+
+            # Create the line for y
+            line2 = go.Scatter(x=date_ls, y=pred, mode='lines', name='Predicted data',line=dict(color='red'))
+
+            # Create the figure and add both lines
+            fig = go.Figure()
+            fig.add_trace(line1)
+            fig.add_trace(line2)
+
+            fig.update_layout(title=f'Prediction of {n} days', xaxis_title='Close Price', yaxis_title='Days')
+            st.plotly_chart(fig)
+
+
+       
+
+
+
